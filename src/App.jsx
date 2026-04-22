0c7f740529de9c554488b111cd2dabc53d669951
Filipe Pimenta Oliveira Monteiro <pimonteiro@protonmail.com>
2026-04-23 00:53:49 +0100

    chore: bump version to v1.0.3 to reflect the deployment of the definitive toolbar visibility block

--- src/App.jsx
+++ src/App.jsx
@@ -158,7 +158,7 @@ export default function App() {
           fontFamily: 'monospace',
           fontWeight: '600'
         }}>
-          v1.0.1
+          v1.0.3
         </div>
       </div>
     </div>
