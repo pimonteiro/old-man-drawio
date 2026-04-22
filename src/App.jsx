dd979a2a0a446adb039f6fde70e0a058a95c5558
Filipe Pimenta Oliveira Monteiro <pimonteiro@protonmail.com>
2026-04-23 00:13:07 +0100

    chore: sync version to v1.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -158,7 +158,7 @@ export default function App() {
           fontFamily: 'monospace',
           fontWeight: '600'
         }}>
-          v1.0.1
+          v1.0.2
         </div>
       </div>
     </div>
